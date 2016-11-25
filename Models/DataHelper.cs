e0feb91befc848dfb30036208c2a6c49bb823c76
Facu Villar <facuandroid.fv@gmail.com>
2016-11-24 23:13:38 -0300

    First Commit
    With test webhook and log

---
+++ Models/DataHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Transactions;
+
+namespace GabrielaMensaqueWebhooks.Models
+{
+    public class DataHelper
+    {
+        private const string ConnectionString = "Server=048cec32-7382-48c5-b195-a6c90016ac5c.sqlserver.sequelizer.com;Database=db048cec32738248c5b195a6c90016ac5c;User ID=yckohfksrmgmrdpo;Password=nRYXzsyKzcpduHRdxmuP2JYZwVcsqBZvjAepejMNPVio6vwhTmgCV5PKZ8ALqgu7;";
+
+        private static SqlCommand DataCommand(string cmd, SqlConnection connection)
+        {
+            return new SqlCommand
+            {
+                CommandType = CommandType.StoredProcedure,
+                CommandTimeout = 60,
+                Connection = connection,
+                CommandText = cmd
+            };
+        }
+
+        public static void ExecuteNonQuery(string cmd, params object[] parameters)
+        {
+            try
+            {
+                using (var scope = new TransactionScope())
+                {
+                    using (var connection = new SqlConnection(ConnectionString))
+                    {
+                        var sqlCommand = DataCommand(cmd, connection);
+                        connection.Open();
+                        SqlCommandBuilder.DeriveParameters(sqlCommand);
+                        if (parameters != null)
+                            if (parameters.Length > 0)
+                            {
+                                var index = 0;
+                                foreach (SqlParameter parameter in sqlCommand.Parameters)
+                                {
+                                    if (parameter.Direction == ParameterDirection.Input ||
+                                        parameter.Direction == ParameterDirection.
+                                            InputOutput)
+                                    {
+                                        parameter.Value = parameters[index];
+                                        index++;
+                                    }
+                                }
+                            }
+                        sqlCommand.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                    scope.Complete();
+                }
+            }
+            catch (SqlException ex)
+            {
+                //LogHelper.Log(ex);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ WebHooks/PayPalHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using Microsoft.AspNet.WebHooks;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using GabrielaMensaqueWebhooks.Models;
+using Newtonsoft.Json;
+
+namespace GabrielaMensaqueWebhooks.WebHooks
+{
+    public class PayPalHandler : PaypalWebHookReceiver
+    {
+        public override Task<HttpResponseMessage> ReceiveAsync(string id, HttpRequestContext context, HttpRequestMessage request)
+        {
+            try
+            {
+                if ("PayPal".Equals(id, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    var jsonContext = JsonConvert.SerializeObject(context);
+                    var jsonRequest = JsonConvert.SerializeObject(request);
+                    DataHelper.ExecuteNonQuery("TestPayPalHandler_Insert", id, jsonContext, jsonRequest);
+                }
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    DataHelper.ExecuteNonQuery("Logs_Insert", ex.ToString());
+                }
+                catch
+                {
+                }
+            }
+            return base.ReceiveAsync(id, context, request);
+        }
+    }
+}
