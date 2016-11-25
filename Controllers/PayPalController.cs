f0c83e7eacbe8777d4c2a4f35e6f131eb0b9cdd5
Facu Villar <facuandroid.fv@gmail.com>
2016-11-25 00:12:00 -0300

    Test Controller
    PayPalController with Post(WebhookEvent evt)

---
+++ Controllers/PayPalController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using GabrielaMensaqueWebhooks.Models;
+using Newtonsoft.Json;
+using PayPal.Api;
+
+namespace GabrielaMensaqueWebhooks.Controllers
+{
+    public class PayPalController : ApiController
+    {
+        // POST: api/PayPal
+        //([FromBody]string value)
+        public void Post(WebhookEvent evt)
+        {
+            try
+            {
+                DataHelper.ExecuteNonQuery("Logs_Insert", JsonConvert.SerializeObject(evt));
+            }
+            catch (Exception ex)
+            {
+                DataHelper.ExecuteNonQuery("Logs_Insert", ex.ToString());
+            }
+        }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace GabrielaMensaqueWebhooks
@@ -12,6 +13,9 @@ namespace GabrielaMensaqueWebhooks
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
